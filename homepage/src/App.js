ebb645e50c1596b7d77608f3f917d17970cf7a94
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-07-09 20:05:25 +0530

    installed react-icons

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import TopPannel from "./pages/TopPannel";
 import LandingPage from "./pages/landingPage";
+import Login from "./pages/LoginRegister"
 
 function App() {
   return (
     <>
       <TopPannel />
       <LandingPage />
+      <Login />
     </>
   );
 }
